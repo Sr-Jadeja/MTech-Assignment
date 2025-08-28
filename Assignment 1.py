53d7fa200a5ce406f0c7c730372404c9035411d0
Sr-Jadeja <ssinhrj@gmail.com>
2025-08-28 21:16:17 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,97 @@
+print('hello')
+hello
+
+# Arithmatic operator
+a=13
+b=6
+c=a+b
+print(c)
+19
+print('sum=,c')
+sum=,c
+print('sum=,'c)
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+print('sum=,c')
+sum=,c
+print('sum=',c)
+sum= 19
+c=a-b
+print('sum=',c)
+sum= 7
+
+a=10
+b=2
+print(a**b)
+100
+a=7
+b=2
+print(a/b)
+3.5
+print(a//b)
+3
+print(-a//b)
+-4
+#a floor b rounds the result to whole number. in negative numbers it rounds toward negative infinity.
+print(10**-1)
+0.1
+print(2**-1)
+0.5
+print(7**0)
+1
+print(7%2)
+1
+
+
+x=3, y=2
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+x=3
+y=2
+print(x==y)
+False
+y=3
+print(x==y)
+True
+print(x>y, x<y, x!=y)
+False False False
+print(x>=y, x<=y)
+True True
+>>> 
+>>> 
+>>> #Assignment operators
+>>> x=7
+>>> x+=3
+>>> print(x)
+10
+>>> x-=3
+>>> print(x)
+7
+>>> x*=3
+>>> print(x)
+21
+>>> x%=3
+>>> print(x)
+0
+>>> x=15
+>>> x/=4
+>>> print(x)
+3.75
+>>> x//=2
+>>> print(x)
+1.0
+>>> x=5378
+>>> x**=3
+>>> print(x)
+155547270152
+>>> 
+>>> 
+>>> a=2
+>>> b=10
+>>> print(a<5 and b>19)
+False
+>>> print(a<5 or b>19)
+True
+>>> is_allowance=False
+>>> print(not is_allowance)
+True
+>>> print(NOT is_allowance)
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
